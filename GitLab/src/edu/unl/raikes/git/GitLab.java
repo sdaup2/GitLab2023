65a0fbd6a21a2158640af503672fb0b7d6d10808
sdaup2 <sdaup2@huskers.unl.edu>
2023-02-03 12:41:01 -0600

    Initial commit including a print statement describing this lab

---
+++ GitLab/src/edu/unl/raikes/git/GitLab.java
@@ -0,0 +1,12 @@
+package edu.unl.raikes.git;
+
+public class GitLab {
+
+    public GitLab() {
+        // TODO Auto-generated constructor stub
+    }
+    public static void main(String args[]) {
+        System.out.print("This is a lesson on git");
+    }
+
+}
