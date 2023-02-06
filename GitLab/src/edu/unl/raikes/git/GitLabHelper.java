386c3691b9075ef171856c6f093cde45726d54bc
ntrejoandalon <ntrejoandalon2@huskers.unl.edu>
2023-02-06 13:05:02 -0600

    Changed GitLabHelper

--- GitLab/src/edu/unl/raikes/git/GitLabHelper.java
+++ GitLab/src/edu/unl/raikes/git/GitLabHelper.java
@@ -9,7 +9,6 @@ public class GitLabHelper {
     }
     
     public void PrintWelcomeMessage() {
-        System.out.print("This is a lesson on git made by " + person1 + " and " +
-            person2);
+        System.out.printf("This is a lesson on git made by %s and %s", person1, person2);
     }
 }
